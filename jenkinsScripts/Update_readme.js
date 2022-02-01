b74cfec93ce4128c3c8e5a975226dd9f8b0b1f6f
root <hachemico@gmail.com>
2022-02-01 17:46:40 +0100

    actualizar

--- jenkinsScripts/Update_readme.js
+++ jenkinsScripts/Update_readme.js
@@ -2,11 +2,9 @@ const fs = require('fs')
 
 
 const testResult = process.argv[2]
-
-
-const success = 'https://img.shields.io/badge/tested%20with-Cypress-04C38E.svg'
-const failed = 'https://img.shields.io/badge/test-failure-red.svg'
-
+console.log(testResult);
+const successBadge = 'https://img.shields.io/badge/tested%20with-Cypress-04C38E.svg'
+const failedBadge = 'https://img.shields.io/badge/test-failure-red.svg'
 
 fs.readFile('README.md', 'utf-8', (err,data) => {
     if (err) {
