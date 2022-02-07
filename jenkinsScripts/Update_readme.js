b1c675d13ec749dcb0c57c08f87980656b162538
root <hachemico@gmail.com>
2022-02-07 20:34:00 +0100

    actualiza

--- jenkinsScripts/Update_readme.js
+++ jenkinsScripts/Update_readme.js
@@ -6,7 +6,7 @@ console.log(testResult);
 const successBadge = 'https://img.shields.io/badge/tested%20with-Cypress-04C38E.svg';
 const failedBadge = 'https://img.shields.io/badge/test-failure-red.svg';
 
-fs.readFile('README.md', 'utf-8', (err,data) => {
+fs.readFile('../README.md', 'utf-8', (err,data) => {
     if (err) {
         throw err
     }
@@ -14,7 +14,7 @@ fs.readFile('README.md', 'utf-8', (err,data) => {
         /(?<=\[!\[Cypress.io\]\()[\s\S]*(?=\)\])/gm,
         testResult == '0' ? successBadge : failedBadge
     )
-    fs.writeFile('README.md', markdownUpdate, 'utf-8', (err) => {
+    fs.writeFile('../README.md', markdownUpdate, 'utf-8', (err) => {
         if (err) {
             console.error(err)
             return
