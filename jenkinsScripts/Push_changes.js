fc238c87df2d4b790722d65e0dba4f57041257fd
root <hachemico@gmail.com>
2022-02-08 15:45:43 +0100

    actualiza

--- jenkinsScripts/Push_changes.js
+++ jenkinsScripts/Push_changes.js
@@ -8,5 +8,5 @@ git config --global user.email \'hachemico@gmail.com\'
 git config --global user.name \'hachemico\'
 git set-url origin https://$TOKEN@github.com/hachemico/practica_jenkins.git
 git add .
-git commit -m "Pipeline ejecutada por $PARAM_EJECUTOR . MOTIVO: $PARAM_MOTIVO "
+git commit -m "Pipeline ejecutada por $PARAM_EJECUTOR . MOTIVO: $PARAM_MOTIVO '
 git push origin main
