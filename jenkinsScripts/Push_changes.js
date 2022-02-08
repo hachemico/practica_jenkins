52ac68836b56f46ff810668175690cea4bde1108
root <hachemico@gmail.com>
2022-02-08 12:00:42 +0100

    actualiar

--- jenkinsScripts/Push_changes.js
+++ jenkinsScripts/Push_changes.js
@@ -1,14 +1,12 @@
-#!/bin/bash
 
 const PARAM_EJECUTOR = process.argv[2];
 const PARAM_MOTIVO = process.argv[3];
 
-console.log(usuario)
-console.log(password)
+console.log(usuario);
+console.log(password);
 
 git config --global user.email \'hachemico@gmail.com\'
-git config --global user.name '\hachemico\'
-
+git config --global user.name \'hachemico\'
 git set-url origin https://github.com/hachemico/practica_jenkins.git
 git add .
 git commit -m 'Pipeline ejecutada por $PARAM_EJECUTOR . MOTIVO: $PARAM_MOTIVO "
