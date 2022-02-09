7a8e72bcfbde2028c9c324fe72eac40a42225959
root <hachemico@gmail.com>
2022-02-09 10:27:23 +0100

    prueba

--- jenkinsScripts/Push_changes.js
+++ jenkinsScripts/Push_changes.js
@@ -1,12 +1,12 @@
-PARAM_EJECUTOR = process.argv[2]
-PARAM_MOTIVO = process.argv[3]
-TOKEN = process.argv[4]
+// PARAM_EJECUTOR = process.argv[2]
+// PARAM_MOTIVO = process.argv[3]
+// TOKEN = process.argv[4]
 
 
 
-git config --global user.email \'hachemico@gmail.com\'
-git config --global user.name \'hachemico\'
-git set-url origin https://$TOKEN@github.com/hachemico/practica_jenkins.git
-git add .
-git commit -m "Pipeline ejecutada por $PARAM_EJECUTOR . MOTIVO: $PARAM_MOTIVO '
-git push origin main
+// git config --global user.email \'hachemico@gmail.com\'
+// git config --global user.name \'hachemico\'
+// git set-url origin https://$TOKEN@github.com/hachemico/practica_jenkins.git
+// git add .
+// git commit -m "Pipeline ejecutada por $PARAM_EJECUTOR . MOTIVO: $PARAM_MOTIVO '
+// git push origin main
---
+++ jenkinsScripts/Vercel.js
@@ -0,0 +1,11 @@
+let res1    = process.argv[2]
+let res2    = process.argv[3]
+let res3    = process.argv[4]
+let token   = process.argv[5]
+
+if((res1== 0)&&(res2==0)&&(res3==0)){
+
+    vercel
+
+
+}
